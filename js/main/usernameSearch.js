da237def5367cd8bad98f29f9bd72685fbd0fd29
luccas-sales <luccassales888@gmail.com>
2025-04-30 16:50:02 -0300

    feat: personalize placeholder with username if available

--- js/main/usernameSearch.js
+++ js/main/usernameSearch.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const username = localStorage.getItem('username');
   const searchInput = document.querySelector('.input');
 
-  if ((username = null)) {
-    searchInput.placeholder = `What’s your next obsession?`;
+  if (username === null) {
+    searchInput.placeholder = 'What’s your next obsession?';
   } else {
     searchInput.placeholder = `What’s your next obsession, ${username}?`;
   }
